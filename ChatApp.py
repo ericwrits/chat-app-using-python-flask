099236e5c40bc77f82b3c595164602edcbd2ecc4
Ezeh Nzube <ezehnzube2015@gmail.com>
2018-11-29 07:45:36 +0100

    chatapp

---
+++ ChatApp.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fgkuhlkdhglfhg8gy4i5'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template("./chatApp.html")
+
+
+@socketio.on('my event')
+def my_event_handler(json):
+    print('received something:' + str(json))
+    socketio.emit('my response', json)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
